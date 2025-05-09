a853e39dbb4098d218e6651a7eb8777519e6575d
gloopmasta <141577881+gloopmasta@users.noreply.github.com>
2025-05-09 22:58:44 +0200

    added UI for turning the bow

--- VR_Bow (Unity Project)/Assets/Scripts/Player/DriveControls/DriveControls.cs
+++ VR_Bow (Unity Project)/Assets/Scripts/Player/DriveControls/DriveControls.cs
@@ -50,8 +50,6 @@ public class DriveControls : MonoBehaviour
     [Header("Scripts & References")]
     [SerializeField] XRControllerData controllerData;
     [SerializeField] BoxCollider groundCollider;
-    [SerializeField] GameObject lockPoint;
-    [SerializeField] GameObject BowMesh;
 
 
     //How much the handle is turned
@@ -72,11 +70,10 @@ public class DriveControls : MonoBehaviour
             UpdateControllerData();
             Steer();
             Drift();
-            LockBowPosition();
         }
 
         //move the player useing rigidbody
-        rb.MovePosition(rb.position + currentVelocity * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + currentVelocity * Time.unscaledDeltaTime);
 
     }
 
@@ -224,7 +221,7 @@ public class DriveControls : MonoBehaviour
                 float targetSteeringInput = Mathf.DeltaAngle(0f, pitch);
 
                 // Smoothly interpolate between current and target steering input
-                currentSteeringInput = Mathf.Lerp(currentSteeringInput, targetSteeringInput, Time.deltaTime * steeringSmoothness);
+                currentSteeringInput = Mathf.Lerp(currentSteeringInput, targetSteeringInput, Time.fixedDeltaTime * steeringSmoothness);
 
                 // Apply rotation - OLD
                 //transform.Rotate(Vector3.up, currentSteeringInput * currentTurnSpeed * Time.deltaTime);
@@ -247,10 +244,6 @@ public class DriveControls : MonoBehaviour
         }
     }
 
-    private void LockBowPosition()
-    {
-        BowMesh.transform.position = lockPoint.transform.position;
-    }
 
     public void TriggerHapticFeedback()
     {
