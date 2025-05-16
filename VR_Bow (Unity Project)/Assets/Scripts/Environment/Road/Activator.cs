3e5be3c5e3a84d040c28bb30993f499aa6e0f1ae
gloopmasta <141577881+gloopmasta@users.noreply.github.com>
2025-05-16 18:50:41 +0200

    Made start screen & fixed road lighting up

---
+++ VR_Bow (Unity Project)/Assets/Scripts/Environment/Road/Activator.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Activator : MonoBehaviour
+{
+    [Header("GameObjects to activate")]
+    [SerializeField] private List<GameObject> objects = new List<GameObject>();
+    [SerializeField] private bool isActivated = false;
+
+    [Header("Animation")]
+    [SerializeField] private Animator animator;
+    [SerializeField] private float timeToDestroy = 1f;
+
+    private void OnEnable()
+    {
+        animator = GetComponent<Animator>();
+        Debug.Assert(animator != null, "No animator on Activator");
+
+        PlaySpawnAnimation();
+    }
+    private void OnDisable()
+    {
+        PlayDespawnAnimation();
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isActivated) return;
+
+        if (other.CompareTag("Player"))
+        {
+            ActivateObjects();
+        }
+    }
+
+    private void ActivateObjects()
+    {
+        foreach (GameObject obj in objects)
+        {
+            if (obj != null || obj.activeSelf) //if object is null or already active
+                obj.SetActive(true);
+        }
+    }
+
+    private void PlaySpawnAnimation()
+    {
+        if (animator != null)
+        {
+            animator.CrossFade("Powerup spawn", 0);
+        }
+    }
+
+    private void PlayDespawnAnimation()
+    {
+        if (animator != null)
+        {
+            animator.CrossFade("Powerup collect", 0);
+        }
+    }
+
+}
---
+++ VR_Bow (Unity Project)/Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,35 @@
+using Cysharp.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager Instance { get; set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this) { Destroy(gameObject); return; }
+        Instance = this;
+    }
+
+    [Header("Start Scene")]
+    public GameObject coverDome;
+    public GameObject startUI;
+
+    [Header("Level one")]
+    public GameObject firstRoad;
+    public GameObject firstActivator;
+
+    public void FinishStartUI()
+    {
+        startUI.SetActive(false);
+        //add animation here
+
+        firstActivator.SetActive(true);
+
+        firstRoad.SetActive(true);
+        //activate skybox
+    }
+}
