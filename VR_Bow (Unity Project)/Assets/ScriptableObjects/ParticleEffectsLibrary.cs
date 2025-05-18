11a1343155b57f382c83ca87f0b3d100e3ece9c4
gloopmasta <141577881+gloopmasta@users.noreply.github.com>
2025-05-18 22:29:45 +0200

    Added more particle effects

--- VR_Bow (Unity Project)/Assets/ScriptableObjects/ParticleEffectsLibrary.cs
+++ VR_Bow (Unity Project)/Assets/ScriptableObjects/ParticleEffectsLibrary.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ParticleEffectsLibrary : MonoBehaviour
+[CreateAssetMenu(menuName = "Game Libraries/Particle Library")]
+public class ParticleEffectsLibrary : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Enemies")]
+    public GameObject arrow;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("Enemies")]
+    public GameObject turretExplode;
 }
