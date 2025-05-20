aebfd42e3979fb426e3532320cca41e9f6a6b664
gloopmasta <141577881+gloopmasta@users.noreply.github.com>
2025-05-20 11:34:06 +0200

    Added art for begin and endscreen

--- Arduino/hall_sensor/hall_sensor.ino
+++ Arduino/hall_sensor/hall_sensor.ino
@@ -1,11 +1,11 @@
 void setup() {
-  Serial.begin(9600); // Start serial at safe speed
-  pinMode(A0, INPUT); // Not strictly necessary but good practice
+  Serial.begin(115200); // Start serial at safe speed
+  pinMode(34, INPUT); // Not strictly necessary but good practice
   Serial.println("Hall Sensor Test - Safe Mode");
 }
 
 void loop() {
-  int sensorValue = analogRead(A0); // Read from A0
+  int sensorValue = analogRead(34); // Read from A0
   float voltage = sensorValue * (5.0 / 1023.0); // Convert to voltage (5V reference)
   
   Serial.print("Raw: ");
