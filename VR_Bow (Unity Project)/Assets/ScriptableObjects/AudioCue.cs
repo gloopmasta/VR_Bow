9a4b5e66b0229840708b6f7f3f43f38477216483
gloopmasta <141577881+gloopmasta@users.noreply.github.com>
2025-05-29 20:24:10 +0200

    Added particle effects for enemies explode

--- VR_Bow (Unity Project)/Assets/ScriptableObjects/AudioCue.cs
+++ VR_Bow (Unity Project)/Assets/ScriptableObjects/AudioCue.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class AudioCue : ScriptableObject
 {
     public AudioClip[] clips;
-    [Range(0f, 1f)] public float volume = 1f;
+    [Range(0f, 10f)] public float volume = 1f;
     [Range(0.1f, 3f)] public float pitch = 1f;
 
     public AudioClip GetRandomClip()
--- VR_Bow (Unity Project)/Assets/ScriptableObjects/ParticleEffectsLibrary.cs
+++ VR_Bow (Unity Project)/Assets/ScriptableObjects/ParticleEffectsLibrary.cs
@@ -1,3 +1,4 @@
+using Cysharp.Threading.Tasks;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,5 +12,9 @@ public class ParticleEffectsLibrary : ScriptableObject
     [Header("Enemies")]
     public GameObject turretExplode;
 
-
+    public void PlayParticle(Vector3 position, GameObject particlePrefab)
+    {
+        var effect = Instantiate(particlePrefab, position, Quaternion.identity);
+        Destroy(effect, 2f);
+    }
 }
